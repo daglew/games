2d295dbd23659d5726087a133c1f327076c0046d
daglew <daga122611@gmail.com>
2024-05-20 23:21:42 +0200

    game_hangman_2

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,35 +1,42 @@
 import sys
+from game_features import GameFeaturesMethod
 
 
-tries = 3
-word = "slowo"
-given_letters = []
+class RunGame(GameFeaturesMethod):
+    def __init__(self, tries, word):
+        super().__init__(tries, word)
 
-user_word = []
+    def run(self):
+        self.create_hashed_user_word()
+        while True:
+            letter = input("Enter a letter: ")
+            self.given_letters.append(letter)
+
+            found_indexes = self.indexes_of_found_letters(letter)
+
+            if len(found_indexes) == 0:
+                print("The wrong answer.")
+                self.tries -= 1
+
+                if self.tries == 0:
+                    print("End of the game.")
+                    sys.exit(0)
+            else:
+                for index in found_indexes:
+                    self.user_word[index] = letter
+
+                if "".join(self.user_word) == self.word:
+                    print("Way to go! You guessed the word.")
+                    sys.exit(0)
+
+            self.current_game_state(self.user_word)
 
 
-def indexes_of_found_letters(word, letter):
-    indexes = []
-    for index, letter_in_word in enumerate(word):
-        if letter == letter_in_word:
-            indexes.append(index)
-    return indexes
-
-
-for _ in word:
-    user_word.append("_")
-
-while True:
-    letter = input("Enter a letter: ")
-    given_letters.append(letter)
-
-    indexes_found = indexes_of_found_letters(word, letter)
-
-    if len(indexes_found) == 0:
-        print("The wrong answer.")
-        tries -= 1
-
-        if tries == 0:
-            print("End of the game.")
-            sys.exit(0)
+if __name__ == "__main__":
+    name = input("Hi! Welcome in my game Hangman. What is your name?: ").capitalize()
+    word = input(f"{name} to start game please set your word!: ")
+    tries = int(input(f"{name} You can define how many tries you need?: "))
+    print("Let's play!")
+    a = RunGame(tries=tries, word=word)
+    a.run()
 
